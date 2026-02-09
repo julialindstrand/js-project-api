1613e9b1c7c26b7c30ffc3d2e07efd9e95706c1f
Julia <juliakeo@pm.me>
2026-02-09 14:57:16 +0100

    More troubleshooting

--- server.js
+++ server.js
@@ -30,28 +30,140 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-
+// Thought schema
 const thoughtSchema = new mongoose.Schema({
   message: { type: String, required: true },
   hearts: { type: Number, default: 0 },
   createdAt: { type: Date, default: Date.now }
+  // userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
 })
 
 const Thought = mongoose.model('Thought', thoughtSchema)
 
-// Start defining your routes here
-app.get("/", async (req, res) => {
-  // const endpoints = listEndpoints(app)
+// User schema
+const UserSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  accessToken: { type: String }
+})
+
+const User = mongoose.model('User', UserSchema)
+
+const authenticateUser = async (req, res, next) => {
   try {
-    const thoughts = await Thought.find()
-    res.json(thoughts)
+    const user = await User.findOne({
+      accessToken: req.header('Authorization').replace("Bearer ", ""),
+    })
+    if (user) {
+      req.user = user
+      next()
+    } else {
+      res.status(401).json({
+        message: "Authentication missing / invalid",
+        loggedOut: true
+      })
+    }
   } catch (error) {
-    console.error(error)
-    res.status(500).json({ message: "Server error" })
+    res.status(500).json({ message: "Internal server error", error: error.message })
+  }
+}
+
+// Start defining your routes here
+
+// All
+app.get("/", (req, res) => {
+  const endpoints = listEndpoints(app)
+  res.json({
+    message: "Welcome to the happy thoughts API. Here is a list of all endpoints",
+    endpoints: endpoints,
+  })
+})
+
+// New User
+app.post('/users/signup', async (req, res) => {
+  try {
+    const { email, password } = req.body
+
+    const existingUser = await User.findOne({
+      email: email.toLowerCase()
+    })
+
+    if (existingUser) {
+      return res.status(400).json({
+        success: false,
+        message: "User with this email already exists"
+      })
+    }
+
+    const salt = bcrypt.genSaltSync()
+    const hashedPassword = bcrypt.hashSync(password, salt)
+    const user = new User({ email, password: hashedPassword })
+
+    await user.save()
+
+    res.status(200).json({
+      success: true,
+      message: "User created successfully",
+      response: {
+        email: user.email,
+        id: user._id,
+        accessToken: user.accessToken,
+      },
+    })
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: 'Could not create user',
+      response: error,
+    })
   }
 })
 
+// Sign In
+app.post('/users/login', async (req, res) => {
+  try {
+    const { email, password } = req.body
+    const user = await User.findOne({ email: email.toLowerCase() })
+
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res.json({
+        success: true,
+        message: "Logged in successfully",
+        response: {
+          email: user.email,
+          id: user._id,
+          accessToken: user.accessToken
+        },
+      })
+    } else {
+      res.status(401).json({
+        success: false,
+        message: "Wrong e-mail or password",
+        response: null,
+      })
+    }
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Something went wrong",
+      response: error
+    })
+  }
+})
+
+// Show all thoughts
 app.get("/thoughts", async (req, res) => {
+  try {
+    const thoughts = await Thought.find().sort({ createdAt: "desc" })
+    res.json(thoughts)
+
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch thougts" })
+  }
+})
+
+// Show thoughts with likes
+app.get("/thoughts/likes", async (req, res) => {
   const { hearts } = req.query
 
   const dbQuery = {}
@@ -87,6 +199,7 @@ app.get("/thoughts", async (req, res) => {
   }
 })
 
+// Filter by ID
 app.get("/thoughts/:id", async (req, res) => {
   const id = req.params.id
 
@@ -126,19 +239,19 @@ app.get("/thoughts/:id", async (req, res) => {
 }
 )
 
-
+// Post
 app.post("/thoughts", async (req, res) => {
-  const { message } = req.body;
+  const { message } = req.body
 
   try {
-    const newThought = await new Thought({ message }).save();
+    const newThought = await new Thought({ message }).save()
 
     if (!newThought) {
       return res.status(400).json({
         success: false,
         data: null,
         message: "Failed to post thought"
-      });
+      })
     }
 
     res.status(201).json({
@@ -151,9 +264,32 @@ app.post("/thoughts", async (req, res) => {
       success: false,
       data: null,
       message: error.message || "Server error"
-    });
+    })
   }
-});
+})
+
+// Edit
+app.patch('/messages/:id', async (req, res) => {
+  try {
+    const thought = await Thought.findById(req.params.id)
+
+    if (!thought) {
+      return res.status(404).json({ error: "Thought not found" })
+    }
+
+    if (thought.userId.toString() !== req.user._id.toString()) {
+      return res.status(403).json({ error: "You can only edit your own thoughts" })
+    }
+
+    thought.thought = req.body.thought || thought.thought
+    await thought.save()
+
+    res.json(thought)
+  } catch (error) {
+    res.status(400).json({ error: "Invalid Id / request" })
+  }
+})
+
 
 // Delete
 app.delete("/thoughts/:id", async (req, res) => {
